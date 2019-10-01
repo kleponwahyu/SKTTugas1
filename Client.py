13d220d311f3ef82e4fee5e64fbda04ee13de345
Wahyu Berlianto <wahyubrr@imajinasi.local>
2019-10-01 19:54:43 +0700

    Removing while loop, makes it simpler

--- Client.py
+++ Client.py
@@ -2,22 +2,16 @@ import zerorpc
 
 c = zerorpc.Client()
 c.connect("tcp://127.0.0.1:10000")
-accepted = 0
-while(1):
-	print ("Masukkan nama user anda:")
-	nama = input()
-	accepted = c.cekNama(nama)
-	if accepted == 1:
-		break
-	else:
-		print("User tidak ditemukan, mohon coba lagi")
 
-amount = 0
-while(1):
+print ("Masukkan nama user anda:")
+nama = input()
+accepted = c.cekNama(nama)
+if accepted == 1:
 	print ("Masukkan Nilai TopUp (Minimal 10000)")
 	amount = input()
 	if int(amount) >= 10000:
-		break
+		print (c.topUp(nama, int(amount)))
 	else:
 		print ("Minimal TopUp sebesar 10000, mohon coba lagi")
-print (c.topUp(nama, int(amount)))
+else:
+	print("User tidak ditemukan, mohon coba lagi")
